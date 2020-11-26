ac5b89ef7f3bd427b6fd30bdc1881d2ae1a50941
Antonin06 <avon.antonin@gmai.com>
2020-11-26 15:35:04 +0100

    V.1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
-import './App.css';
+import './Assets/Css/Core/App.css';
 
 const App = () => (
   <ReactFullpage
   //fullpage options
   /* Options here */
-  scrollingSpeed = {1000}
-  navigation = "true" 
-  anchors={['Home', 'AboutMe', 'Contact']}
-  navigationTooltips={["Home","About me", "Contact"]}
+  scrollingSpeed = {1000} //Vitesse de scrolling
+  navigation = "true" //Ajoute les points de navigation
+  anchors={['Home', 'AboutMe', 'Contact']} //Montre dans la barre d'url le #Home/AboutMe/Contact. Commenter cette ligne pour supprimer option.
+  navigationTooltips={["Home","About me", "Contact"]} //Nom des sections dans l'application
+  showActiveTooltip = "true" //Affiche les noms des sections dans la navigation
+  controlArrows = {false} //Supprime les fleches de directions sur les slides horizontales.
+  slidesNavigation = "true" //Ajoute les points de navigation pour les slides
 
     render={({ state, fullpageApi }) => {
       return (
         <ReactFullpage.Wrapper>
           <div className="section s1">
-            <p>Section 1 (welcome to fullpage.js)</p>
-            <button onClick={() => fullpageApi.moveSectionDown()}>
-              Click me to move down
-            </button>
+            <p>Section 1 (welcome to fullpage.js)</p>            
           </div>
           <div className="section s2">
-            <p>Section 2</p>
+            <div className="slide">
+              <p>Slide 1 </p>
+            </div>
+            <div className="slide">
+              <p>Slide 2</p>
+            </div>
+            <div className="slide">
+              <p>Slide 3</p>
+            </div>
           </div>
           <div className="section s3">
             <p>Section 3</p>
