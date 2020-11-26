1fd479228b90ea1e15581b1e5e06e2f1ba55b73e
Antonin06 <avon.antonin@gmai.com>
2020-11-26 14:43:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactFullpage from '@fullpage/react-fullpage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <ReactFullpage
+  //fullpage options
+  /* Options here */
+  scrollingSpeed = {1000}
+  navigation = "true" 
+  anchors={['Home', 'AboutMe', 'Contact']}
+  navigationTooltips={["Home","About me", "Contact"]}
+
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section s1">
+            <p>Section 1 (welcome to fullpage.js)</p>
+            <button onClick={() => fullpageApi.moveSectionDown()}>
+              Click me to move down
+            </button>
+          </div>
+          <div className="section s2">
+            <p>Section 2</p>
+          </div>
+          <div className="section s3">
+            <p>Section 3</p>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
 
 export default App;
